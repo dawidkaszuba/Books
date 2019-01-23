4f793b0a6c65236c07506bc847a68bff06124abb
dawidkaszuba <d.kaszuba89@gmail.com>
2019-01-23 23:06:44 +0100

    table with books/ div with details/ div toggle

--- js/app.js
+++ js/app.js
@@ -1,10 +1,36 @@
 $(function(){
 
+
+
   $.get("http://localhost:8282/books").done(function(books) {
         for (var book of books) {
-            console.log(book);
+            addBook(book);
         }
     });
+
+
+  function addBook(book){
+  	var newRow = $(`<tr class="title"><td>${book.title}</td><td>${book.author}
+  		</td><td><a href="#" data-id="${book.id}" data-method="DELETE">Delete
+  		</a></td></tr>`);
+  	$(".table").append(newRow);
+
+  	var divUnderTitle = $(`<div class="details" style="display: none ">
+  								isbn:${book.isbn}
+  								publisher: ${book.publisher}
+  								type: ${book.type}
+  							</div>`);
+  	$(".table tr").last().after(divUnderTitle);
+
+  };
+
+  var table = $(".table");
+  table.on("click", "tr", function(){
+  	
+  	$(this).next().toggle();
+
+  });
+
 
 
 
